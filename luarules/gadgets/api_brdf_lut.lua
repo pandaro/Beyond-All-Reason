8e132cc6dc43783732bea01a0a3e6278856647a4
LHoG <ccna@mail.ru>
2019-03-10 04:10:13 +0300

    Added BRDF LUT generation. Will be used by all PBR implementations

---
+++ luarules/gadgets/api_brdf_lut.lua
@@ -0,0 +1,49 @@
+function gadget:GetInfo()
+	return {
+		name      = "BRDF LUT Generator",
+		desc      = "Generates BRDF Lookup table for PBR shaders",
+		author    = "ivand",
+		date      = "2019",
+		license   = "PD",
+		layer     = -1,
+		enabled   = true,
+	}
+end
+
+if (not gadgetHandler:IsSyncedCode()) then --unsynced gadget
+	local oneTime = true
+	local genLut
+
+	local BRDFLUT_TEXDIM = 512 --512 is BRDF LUT texture resolution
+	
+	local function GetBrdfTexture()
+		return genLut:GetTexture()
+	end
+
+	function gadget:DrawGenesis()
+		if oneTime then
+			if genLut then
+				genLut:Execute(false)
+				GG.GetBrdfTexture = GetBrdfTexture
+			end
+			oneTime = false
+		end
+	end
+
+	function gadget:Initialize()
+		local genLutClass = VFS.Include("Luarules/Gadgets/Include/GenBrdfLut.lua")
+		if genLutClass then
+			genLut = genLutClass(BRDFLUT_TEXDIM)
+			if genLut then
+				genLut:Initialize()
+			end
+		end
+	end
+
+	function gadget:Shutdown()
+		if genLut then
+			genLut:Finalize()
+		end
+	end
+end
+
